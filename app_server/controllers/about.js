d1c6ea382d9aac8b344429d929a4276641c18135
Alejandro Duque <aduquead@gmail.com>
2021-11-16 18:40:35 -0500

    Proyecto 1

--- app_server/controllers/about.js
+++ app_server/controllers/about.js
@@ -3,12 +3,12 @@ const about = (req, res) => {
     res.render('about', { 
         title: 'Sobre nosotros' ,
         description: 'Conoce mas sobre ByE',
-        about_img: 'https://scontent.fuio1-2.fna.fbcdn.net/v/t1.6435-9/120504052_2209107295900331_1553419044402864033_n.jpg?_nc_cat=101&ccb=1-5&_nc_sid=84a396&_nc_ohc=MRO1cyi_bZMAX98Hs7Q&_nc_ht=scontent.fuio1-2.fna&oh=fe694f2460911b45fadddfc3e950c69e&oe=61925E57',
+        about_img: '/images/perfil.jpg',
         texto: 'ByE Bonitos y Esponjositos es una tienda online ecuatoriana, fundada por dos hermanas que disfrutan del arte, la comida y la salud. La tienda fue fundada en el año 2020 con el objetivo de aprovechar el conocimiento del tejido de estos peluches ',
        
         title2: 'Nuestros Productos',
         description2:'Conoce cuales son nuestros productos',
-        image_product: 'https://scontent.fuio1-2.fna.fbcdn.net/v/t1.6435-9/145956349_355288232169020_8931413860964012625_n.png?_nc_cat=107&ccb=1-5&_nc_sid=730e14&_nc_ohc=7zMXqBP8TVwAX9hbfcp&_nc_ht=scontent.fuio1-2.fna&oh=aa19fe6345b8b2f81f92d3972717ef93&oe=6190A262',
+        image_product: '/images/catalogo.png',
         'productos' : [{
          nombre_producto: 'Peluches crochet personalizados, por stock o bajo pedido'
           },{
