7cfd8f779a49b873ed26a308b1472f585ac93ac8
grimloc-aduque <aduquead13@gmail.com>
2021-11-20 08:57:16 -0500

    Cambios en formularios y home page

--- app_server/controllers/home.js
+++ app_server/controllers/home.js
@@ -1,12 +1,33 @@
 /* GET home page. */
 const home = (req, res) => {
     res.render('home', {
-        title: 'ByE' ,
+        title: 'Bienvenidos a ByE' ,
+        carrusel1: (src = '/images/carrusel1.png'),
+        carrusel2: (src = '/images/carrusel2.png'),   
+        carrusel3: (src = '/images/carrusel3.png'),
         pageHeader: {
             welcome: 'BIENVENIDOS A',
             title:'ByE',
             strapline: 'Bonitos y Esponjositos'
-        }
+        },
+        'categorias' : [{
+            imagen: (src = '/images/peluches.png'),
+            nombre: 'Peluches',
+            descripcion:"Encuentra un peluche para ti"
+        },{
+            imagen: (src = '/images/skincare.png'),
+            nombre: 'Skincare',
+            descripcion:"Cuida tu piel con nosotros"
+        },{
+            imagen: (src = '/images/golosinas.png'),
+            nombre: 'golosinas',
+            descripcion:"Encuentra combos de golosinas para ti"
+            
+        }],
+        
+         
+        
+        
     });
 };
 
